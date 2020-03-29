d82531c0971238bb3995fcf0c81691ba00ea22e1
Malcolm Navarro <malcolmcyber@gmail.com>
2020-03-29 16:40:04 -0500

    Impl add facility form to facility_dev db

--- src/app/containers/Facilities/index.jsx
+++ src/app/containers/Facilities/index.jsx
@@ -4,12 +4,14 @@ import { Switch, Route, Redirect } from "react-router-dom";
 // Containers
 import AllFacilities from "./All";
 import Facility from "./Facility";
+import AddFacility from "./Add";
 
 
-const Facilities = ({ match: { url }}) => (
+const Facilities = ({ match: { url } }) => (
   <Switch>
     <Route path={`${url}/all`} component={AllFacilities} />
     <Route path={`${url}/provider/:providerId`} component={Facility} />
+    <Route path={`${url}/add`} component={AddFacility} />
     <Redirect to={`${url}/all`} />
   </Switch>
 );
