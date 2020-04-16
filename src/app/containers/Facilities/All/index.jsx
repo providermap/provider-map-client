9943892ae5d436dfab75d427f632f29f8b5d307d
Malcolm Navarro <malcolmcyber@gmail.com>
2020-04-15 19:51:35 -0500

    Disable filters when facilities are loading

--- src/app/containers/Facilities/All/index.jsx
+++ src/app/containers/Facilities/All/index.jsx
@@ -89,12 +89,12 @@ const AllFacilities = () => {
 
             {/* Filters */}
             <Col md="4">
-              <Controller as={Select} control={control} name="facilityType" label="Facility Type" small>
+              <Controller as={Select} control={control} name="facilityType" label="Facility Type" disabled={isLoading} small>
                 { facilityTypes.map((facilityType) => <option key={facilityType} value={facilityType}>{ facilityType }</option>) }
               </Controller>
             </Col>
             <Col md="4">
-              <Controller as={Select} control={control} name="traumaType" label="Trauma Type" small>
+              <Controller as={Select} control={control} name="traumaType" label="Trauma Type" disabled={isLoading} small>
                 { traumaTypes.map((traumaType) => <option key={traumaType} value={traumaType}>{ traumaType }</option>) }
               </Controller>
             </Col>
